ab6bdafcca4f67615073cffc19286031b7c8a1dd
poyingHAHA <poying.code@gmail.com>
2022-11-12 19:38:22 +0800

    a few modifications

--- compile.js
+++ compile.js
@@ -2,13 +2,13 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
  
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
  
 const input = {
   language: 'Solidity',
   sources: {
-    'Inbox.sol': {
+    'Lottery.sol': {
       content: source,
     },
   },
@@ -21,5 +21,4 @@ const input = {
   },
 };
  
-module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Inbox.sol'].Inbox;
-// module.exports = solc.compile(source, 1).contracts[":Inbox"]; // Get access to the bytecode of our contract
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Lottery.sol'].Lottery;
