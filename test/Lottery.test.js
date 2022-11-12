31993f95d21e5d599705c487279828bbac7b29f6
poyingHAHA <poying.code@gmail.com>
2022-11-12 20:47:43 +0800

    testing pickWinner function

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -71,4 +71,15 @@ describe('Lottery Contract', () => {
             assert(err); // Because the value is too less, it's going to throw error
         }
     });
+
+    it('only manager can call pickWinner', async() => {
+        try{
+            await lottery.methods.pickWinner().send({
+               from: accounts[1] 
+            });
+            assert(false);
+        }catch(err){
+            assert(err);
+        }
+    });
 });
